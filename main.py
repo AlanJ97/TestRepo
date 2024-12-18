d61321bc664decd230e0fc2d4e5daefa8b811634
Alan <alannavaseg_021@hotmail>
2024-12-18 15:47:29 -0600

    versao 2

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print ("My APP 1")
+
+# Test case for the print statement
+def test_print_output(capsys):
+    print("My APP 1")
+    captured = capsys.readouterr()
+    assert captured.out == "My APP 1\n"
